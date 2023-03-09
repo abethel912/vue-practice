57986f7bf565a59f8fd3c888b8e1867d9688a306
abethel912 <abethel180@gmail.com>
2023-03-08 19:29:35 -0500

    Added Header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="container">
+    <Header />
   <h1>Hello World</h1>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+
+};
+</script>
+
 
 <style> 
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
